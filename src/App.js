12b9241acd60cb2eed1689dd1cfeff27007ab7bd
Utkarsh Patiyal <utkarshpatiyal99@gmail.com>
2021-07-11 12:02:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Home from "./Home.js";
+
+function App() {
+  return (
+    <div className="app">
+      {/* Home */}
+      <Home />
+      {/* Header */}
+
+      {/* Banner */}
+
+      {/* Cards */}
+
+      {/* Footer */}
+
+      {/* Search Page */}
+    </div>
+  );
+}
+
+export default App;
